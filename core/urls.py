b6614511020eddc3a3aee48bafa7873fca9cb635
Lochlair <john.endahl@icloud.com>
2025-12-02 11:04:41 -0500

    Added the htmx to begin a live-search of composers. Added the needed templates and urls, as well as the results partial.

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search_page, name='search_page'),
     path('counter/', views.counter, name='counter'),
+    path('search/composers', views.search_composers, name='search_composers'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Composer
+from django.db.models import Q
 
 # Create your views here.
 def home(request):
@@ -19,3 +21,29 @@ def counter(request):
     """
 
     return HttpResponse(html)
+
+# This page will load only the search UI and htmx components.
+def search_page(request):
+    return render(request, "search_page.html")
+
+def search_composers(request):
+    q = request.GET.get("q", "").strip()
+
+    composers = Composer.objects.all()
+
+    if q:
+        terms = q.split()
+
+        for term in terms:
+            composers = composers.filter(
+                Q(first_name__icontains=term) |
+                Q(last_name__icontains=term)
+            )
+
+        composers = composers.order_by("last_name", "first_name")
+    else:
+        composers = []
+
+    return render(request, "partials/composer_results.html", {
+        "composers": composers,
+    })
